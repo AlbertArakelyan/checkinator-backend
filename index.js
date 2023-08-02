f987fb8c157866c789016164a5df4f2989eba0b8
Albert Arakelyan <albertarakelyan1998@gmail.com>
2023-08-02 21:47:04 +0300

    feat(3): connected mongodb

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import * as dotenv from 'dotenv';
 
+// DB
+import connectDB from './db.js';
+
+
+dotenv.config();
 
 const app = express();
+
+// Global usages
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello from Checkinator\'s powerful server</h1>');
 });
 
-const startServer = () => {
-  app.listen(5000, () => {
-    console.log('Server is running on port 5000');
-  });
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(5000, () => {
+      console.log('Server is running on port 5000');
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 startServer();
